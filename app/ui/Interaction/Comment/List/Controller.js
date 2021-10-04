7c31935b523c25fcb11ee68c352ae8fc4aba8304
Pol Tran <tranvuminhtriet@gmail.com>
2021-10-04 17:07:10 +0700

    fix query

--- app/ui/Interaction/Comment/List/Controller.js
+++ app/ui/Interaction/Comment/List/Controller.js
@@ -1,19 +1,14 @@
 import {gql, useQuery} from "@apollo/client";
 export const COMMENT_LIST = gql`
-query($id: ID!){
-    Post(where: {id: $id}){
-        interactive{
-            comments{
-                id
-                content
-            }
-        }
-    }
-
+query($id: ID!) {
+  allInteractiveComments(where: { interactive: { post: { id: $id } } }) {
+    id
+    content
+  }
 }
 `;
 export function CommentList(UI, id, ...props){
-    if(!id) return 'id is required!';
+    if(!id) return 'ID is required!';
  const {loading, error, data = {}, refetch}= useQuery(COMMENT_LIST,{variables:postId});
  const { commentList = [] } = data.Post.interactive.comments;
     return (
