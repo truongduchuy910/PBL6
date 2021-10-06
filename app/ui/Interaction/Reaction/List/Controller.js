4dcd0eca681b68a81976ab11b3c4ffb21602657a
Pol Tran <tranvuminhtriet@gmail.com>
2021-10-06 11:14:54 +0700

    11:10 6/10 fix type whereInput

--- app/ui/Interaction/Reaction/List/Controller.js
+++ app/ui/Interaction/Reaction/List/Controller.js
@@ -1,23 +1,17 @@
 import { gql } from "@apollo/client";
 export const REACTION_LIST = gql`
-  query($id: ID!) {
-    allInteractiveReactions(where: { interactive: { post: { id: $id } } }) {
+  query($where: InteractiveReactionWhereInput) {
+    allInteractiveReactions(where: $where ) {
       id
       emoji
     }
   }
 `;
-export default function ReactionList({ UI, postId }) {
-  if (!postId) return "ID required!";
+export default function ReactionList({ UI, id }) {
+  if (!id) return "ID required!";
   const { loading, error, data = {} } = useQuery(REACTION_LIST, {
-    variables: { id: postId },
+    variables: { where: { interactive: { post: { id: $id } } } },
   });
   const { allReactions = [] } = data;
-  return (
-    <UI
-      loading={loading}
-      error={error}
-      allReactions={allReactions}
-    />
-  );
+  return <UI loading={loading} error={error} allReactions={allReactions} />;
 }
