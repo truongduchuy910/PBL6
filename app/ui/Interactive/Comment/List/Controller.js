9848e2d3baf296e83971d6f07519ef870d0b597b
Pol Tran <tranvuminhtriet@gmail.com>
2021-10-19 21:00:05 +0700

    query posts, comments sortBy: createdAt_DESC

--- app/ui/Interactive/Comment/List/Controller.js
+++ app/ui/Interactive/Comment/List/Controller.js
@@ -1,28 +1,45 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
+import { gql, makeVar, useQuery } from "@apollo/client";
 export const COMMENT_LIST = gql`
-  query($first: Int, $skip: Int, $where: InteractiveCommentWhereInput) {
+  query(
+    $first: Int
+    $sortBy: [SortInteractiveCommentsBy!]
+    $skip: Int
+    $where: InteractiveCommentWhereInput
+  ) {
     _allInteractiveCommentsMeta(where: $where) {
       count
     }
-    allInteractiveComments(first: $first, skip: $skip, where: $where) {
+    allInteractiveComments(
+      first: $first
+      skip: $skip
+      sortBy: $sortBy
+      where: $where
+    ) {
       id
       content
     }
   }
 `;
+export const CommentListRefetch = makeVar(() => {});
+
 export function CommentListController({
   UI,
-  first,
+  first = 5,
+  sortBy = "createdAt_DESC",
   skip,
   where,
   ...props
 }) {
-  const { loading, error, data = {}, refetch } = useQuery(COMMENT_LIST, {
-    variables: { first, skip, where },
-  });
+  const { loading, error, data = {}, fetchMore, refetch } = useQuery(
+    COMMENT_LIST,
+    {
+      variables: { first, skip, sortBy, where },
+    }
+  );
   const { _allInteractiveCommentsMeta = {}, allInteractiveComments } = data;
   const { count } = _allInteractiveCommentsMeta;
+  if (refetch) CommentListRefetch(refetch);
   return (
     <UI
       {...props}
