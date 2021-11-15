4b2c1e7f6294e5e55aff0771039e99d5450b52aa
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-11-15 19:55:01 +0700

    status-button-relationship

--- app/pages/User.js
+++ app/pages/User.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container } from "native-base";
 import { UserItemDetail } from "../ui/User";
 import { useRoute } from "@react-navigation/core";
+import { AuthContext } from "../ui/Provider/Native";
 
 export default function User({ navigation }) {
+  const { user } = useContext(AuthContext);
   const { params = {} } = useRoute();
   const { id } = params;
   return (
     <Container w="container.lg" margin="auto" mt="64px" maxW="full" px="8px">
-      <UserItemDetail id={id} />
+      <UserItemDetail id={id} my_id={user?.id} />
     </Container>
   );
 }
--- app/ui/User/Item/Controller.js
+++ app/ui/User/Item/Controller.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { POST_LIST } from "../List/Controller";
-export const POST_ITEM = gql`
+export const POST_ITEM_ME = gql`
   query($id: ID!) {
     User(where: { id: $id }) {
       id
@@ -16,38 +16,54 @@ export const POST_ITEM = gql`
     }
   }
 `;
-export default function UserItem({ UI, where, id }) {
-  const { loading, error, data = {} } = useQuery(id ? POST_ITEM : POST_LIST, {
-    variables: id ? { id } : { where },
-    //variables: id ? { id: id, my_id: currentUser.id } : { where, my_id: currentUser.id }
-  });
+export const POST_ITEM = gql`
+  query($id: ID!, $my_id: ID!) {
+    User(where: { id: $id }) {
+      id
+      phone
+      name
+      avatar {
+        publicUrl
+      }
+    }
+    allPosts(where: { createdBy: { id: $id } }) {
+      id
+    }
+    allRelationships(
+      where: {
+        OR: [
+          { OR: { createdBy: { id: $my_id }, to: { id: $id } } }
+          { OR: { createdBy: { id: $id }, to: { id: $my_id } } }
+        ]
+      }
+    ) {
+      id
+      isAccepted
+      createdBy {
+        id
+      }
+      to {
+        id
+      }
+    }
+  }
+`;
+export default function UserItem({ UI, where, id, my_id }) {
+  const { loading, error, data = {} } = useQuery(
+    id && my_id ? POST_ITEM : id ? POST_ITEM_ME : POST_LIST,
+    {
+      variables:
+        id && my_id ? { id: id, my_id: my_id } : id ? { id } : { where },
+    }
+  );
   const { allUsers, User } = data;
   const [user] = allUsers || [User];
   const { allPosts = [] } = data;
-  return <UI loading={loading} error={error} user={user} posts={allPosts} />;
+  const { allRelationships = [] } = data;
+  var relationship;
+  if (allRelationships.length === 0) { relationship = null }
+  else {
+    relationship = allRelationships[0]
+  }
+  return <UI loading={loading} error={error} user={user} posts={allPosts} relationship={relationship} />;
 }
-// export const POST_ITEM = gql`
-//   query($id: ID!, $my_id: ID!) {
-//     User(where: { id: $id }) {
-//       id
-//       phone
-//       name
-//       avatar {
-//         publicUrl
-//       }
-//     }
-//     allPosts(where: { createdBy: { id: $id } }) {
-//       id
-//     }
-//     allRelationships(where: { createdBy: { id: $my_id }, to: { id: $id } }) {
-//       id
-//       isAccepted
-//       createdBy {
-//         id
-//       }
-//       to {
-//         id
-//       }
-//     }
-//   }
-// `;
